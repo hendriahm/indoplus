0a7a6ba4a678b8b25f8cfb1756c02460317f4594
Hendri Ahmadian <hendri.chan@gmail.com>
2023-06-16 17:14:25 +0700

    commit

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -888,28 +888,26 @@ class NewsCategorizationDataLoader(DataLoader):
             seq_list.append(raw_seq)
             
         return subword_batch, mask_batch, labels_batch, seq_list
-    ####
-    #BERT and BiLSTM (CustomBERTModel)
-    ####
+####
+#BERT and BiLSTM (CustomBERTModel)
+####
 
 class CustomBERTModel(nn.Module):
     def __init__(self):
-          super(CustomBERTModel, self).__init__()
-          self.bert = BertModel.from_pretrained("bert-base-uncased")
-          ### New layers:
-          self.lstm = nn.LSTM(768, 256, batch_first=True,bidirectional=True)
-          self.linear = nn.Linear(256*2, 5)
+        super(CustomBERTModel, self).__init__()
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
+        ### New layers:
+        self.lstm = nn.LSTM(768, 256, batch_first=True,bidirectional=True)
+        self.linear = nn.Linear(256*2, 5)
           
     def forward(self, ids, mask):
-          sequence_output, pooled_output = self.bert(
-               ids, 
-               attention_mask=mask)
+        sequence_output, pooled_output = self.bert(ids, attention_mask=mask)
 
-          # sequence_output has the following shape: (batch_size, sequence_length, 768)
-          lstm_output, (h,c) = self.lstm(sequence_output) ## extract the 1st token's embeddings
-          hidden = torch.cat((lstm_output[:,-1, :256],lstm_output[:,0, 256:]),dim=-1)
-          linear_output = self.linear(hidden.view(-1,256*2)) ### assuming that you are only using the output of the last LSTM cell to perform classification
+        # sequence_output has the following shape: (batch_size, sequence_length, 768)
+        lstm_output, (h,c) = self.lstm(sequence_output) ## extract the 1st token's embeddings
+        hidden = torch.cat((lstm_output[:,-1, :256],lstm_output[:,0, 256:]),dim=-1)
+        linear_output = self.linear(hidden.view(-1,256*2)) ### assuming that you are only using the output of the last LSTM cell to perform classification
 
-          return linear_output
+        return linear_output
     tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
-model = CustomBERTModel()
+    model = CustomBERTModel()
